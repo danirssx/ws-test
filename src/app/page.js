543dff52fe52b7e812a31640c10baff1c8f39288
Daniel Ross <98921448+danirssx@users.noreply.github.com>
2024-01-25 21:48:11 +0000

    iPad things

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+      <div>                   
         There's a difference jeje
       </div>
+       <div className="p-10">
+       Hola 
+       </div>     
       <div className="p-5 backdrop-blur-0 to-blue-300 top-2">
-        <div>
-          Daniel
-        </div>
+       Danil
 
       </div>
     </main>
   );
 } 
-
+  
